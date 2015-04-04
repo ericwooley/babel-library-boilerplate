7d66d4b9dab0d669ea1ea77858a36d1645f0486a
Eric Wooley <ericwooley@gmail.com>
2015-04-04 12:11:51 -0700

    changed to copy just the babel stuff

--- app/index.js
+++ app/index.js
@@ -9,49 +9,21 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   prompting: function () {
-    var done = this.async();
+    // var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the luminous ' + chalk.red('BabelLib') + ' generator!'
     ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
   },
 
   writing: {
     app: function () {
       this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
+        this.templatePath('*'),
+        this.destinationPath('')
       );
     },
-
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
   },
 
   install: function () {
